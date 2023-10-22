b8fbbcbd31ad0412df448dd4c8f60d9acdc10a76
Nandini <Nandini@gmail.com>
2023-10-22 15:22:23 +0530

    Day 1 Code

---
+++ Day1/Code examples/List.py
@@ -0,0 +1,10 @@
+# Creating a List with
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5]
+print("Original list ", List)
+
+List[3] = 77
+print("Example to show mutability ", List)
+
+#Mutable objects are those that allow you to change their value or data in 
+# place without affecting the object's identity. 
+# In contrast, immutable objects don't allow this kind of operation
---
+++ Day1/Code examples/tuple.py
@@ -0,0 +1,5 @@
+# code to test that tuples are immutable
+
+tuple1 = (0, 1, 2, 3)
+tuple1[0] = 4
+print(tuple1)
